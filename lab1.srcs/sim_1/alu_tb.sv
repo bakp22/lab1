2bf3b1c63dfb13b7d7dfeffee903b5b8de67cfd9
Beren Akpinar <101152896+bakp22@users.noreply.github.com>
2025-09-26 03:15:48 -0400

    Added test cases for ADD and SUB and change control bit

--- lab1.srcs/sim_1/alu_tb.sv
+++ lab1.srcs/sim_1/alu_tb.sv
@@ -25,7 +25,7 @@ module alu_tb();
     parameter OP_WIDTH = 4;
     reg [OP_WIDTH-1:0] op1;
     reg [OP_WIDTH-1:0] op2;
-    reg [2:0] control;
+    reg [3:0] control;
     wire [OP_WIDTH-1:0] res;
 
     alu #(.OP_WIDTH(OP_WIDTH)) my_alu(
@@ -37,45 +37,55 @@ module alu_tb();
 
     initial begin
         //NOT
-        op1 = 4'b0000; control = 3'b000; 
+        op1 = 4'b0000; control = 4'b0000; 
         #10; // time for res to update
         assert (res === ~op1) else $finish;
         $display("NOT: %d -> %d", res, op1);
         //XOR
-        op1 = 4'b0001; op2 = 4'b0000; control = 3'b001;
+        op1 = 4'b0001; op2 = 4'b0000; control = 4'b0001;
         #10; 
         assert (res === op1 ^ op2) else $finish;
         $display("XOR: %d -> %d", res, op1);
         //AND
-        op1 = 4'b0001; op2 = 4'b0001; control = 3'b010; 
+        op1 = 4'b0001; op2 = 4'b0001; control = 4'b0010; 
         #10; 
         assert (res === op1 & op2) else $finish;
         $display("AND: %d", res);
         //OR
-        op1 = 4'b0001; op2 = 4'b0001; control = 3'b011;
+        op1 = 4'b0001; op2 = 4'b0001; control = 4'b0011;
         #10;
         assert (res === op1 | op2) else $finish;
         $display("OR:  %d", res);
         //XNOR
-        op1 = 4'b0001; op2 = 4'b0001; control = 3'b100;
+        op1 = 4'b0001; op2 = 4'b0001; control = 4'b0100;
         #10;
         assert (res === ~(op1^op2)) else $finish;
         $display("XNOR: %d", res);
         //Left Shift
-        op1 = 4'b0011; op2 = 4'b0010; control = 3'b101;
+        op1 = 4'b0011; op2 = 4'b0010; control = 4'b0101;
         #10;
         assert (res === (op1 << op2)) else $finish;
         $display("LEFT_SHIFT: %d", res);
         //Right Shift
-        op1 = 4'b1100; op2 = 4'b0010; control = 3'b110;
+        op1 = 4'b1100; op2 = 4'b0010; control = 4'b0110;
         #10;
         assert (res === (op1 >> op2)) else $finish;
         $display("RIGHT_SHIFT: %d", res);
         //RIGHT Shift signed
-        op1 = 4'b1100; op2 = 4'b0010; control = 3'b111;
+        op1 = 4'b1100; op2 = 4'b0010; control = 4'b0111;
         #10;
         assert (res === (op1 >>> op2)) else $finish;
         $display("RIGHT_SHIFT_SIGNED: %d", res);
+        //SUB
+        op1 = 4'b1100; op2 = 4'b0010; control = 4'b1111;
+        #10;
+        assert (res === (op1-op2)) else $finish;
+        $display("SUB: %d", res);
+        //ADD
+        op1 = 4'b1100; op2 = 4'b0010; control = 4'b1000;
+        #10;
+        assert (res === (op1+op2)) else $finish;
+        $display("ADD: %d", res);
         
     end
     
